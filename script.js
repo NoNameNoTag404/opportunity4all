92d5c7f21a67ebdfbe977472252b0ad70b98a0d0
Villanueva-Rayshawn <villanuevarayshawnmark@gmail.com>
2026-02-12 18:34:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,10 @@ behavior:"smooth"
 });
 });
 });
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
